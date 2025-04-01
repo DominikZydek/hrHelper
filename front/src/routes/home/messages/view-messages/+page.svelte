7ac2a187a1f451d5e6e49c98f74d6c4b602110c2
Dominik Żydek <dominikzydek@gmail.com>
2025-04-01 12:24:52 +0200

    Added search mapper for message search

--- front/src/routes/home/messages/view-messages/+page.svelte
+++ front/src/routes/home/messages/view-messages/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import MessageList from '../../../../components/MessageList.svelte';
 	import Searchbar from '../../../../components/Searchbar.svelte';
+	import { getMessagePriorityInfo } from '../../../../utils/getMessagePriorityInfo.js';
 
 	let { data } = $props();
 
@@ -13,10 +14,29 @@
 		displayedMessages = filteredData;
 	};
 
-	const searchMapper = () => {};
+	const searchMapper = (message) => {
+		let groups = [];
+
+		message.author.groups.forEach((g) => {
+			groups.push(g.name);
+		});
+
+		return {
+			first_name: message.author.first_name,
+			last_name: message.author.last_name,
+			full_name: message.author.first_name + ' ' + message.author.last_name,
+			email: message.author.email,
+			job_title: message.author.job_title,
+			groups: groups,
+			priority: getMessagePriorityInfo(message.priority).message,
+			subject: message.subject,
+			publication_date_locale_date: new Date(message.publication_date).toLocaleDateString(),
+			publication_date_locale_time: new Date(message.publication_date).toLocaleTimeString()
+		};
+	};
 </script>
 
-<div class="flex-1 p-4">
+<div class="flex-1 p-4 overflow-auto">
 	<div class="flex items-center gap-8 mb-4">
 		<p class="font-semibold text-2xl text-main-app">Wiadomości</p>
 		<div class="flex-1">
@@ -24,6 +44,7 @@
 				placeholderText="Szukaj wiadomości..."
 				searchData={data.me.visibleMessages}
 				onFilteredDataChange={handleFilteredDataChange}
+				{searchMapper}
 			/>
 		</div>
 	</div>
